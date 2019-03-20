77c47cf15f717e8d9cfdac29d40a38308ce3fffa
Ben <ben@stolovitz.com>
2019-03-19 19:16:30 -0700

    Updates

--- js/main.js
+++ js/main.js
@@ -1,15 +1,17 @@
 const menu_button = document.getElementById("menu_button");
 const nav = document.getElementById("main_nav");
 
-menu_button.onclick = function()
-{
-	const is_active = (menu_button.getAttribute("aria-checked") == "true");
+if(menu_button) {
+	menu_button.onclick = function()
+	{
+		const is_active = (menu_button.getAttribute("aria-checked") == "true");
 
-	menu_button.setAttribute("aria-checked", !is_active);
+		menu_button.setAttribute("aria-checked", !is_active);
 
-	if(is_active) {
-		main_nav.classList.remove("active");
-	} else {
-		main_nav.classList.add("active");
+		if(is_active) {
+			main_nav.classList.remove("active");
+		} else {
+			main_nav.classList.add("active");
+		}
 	}
 }
