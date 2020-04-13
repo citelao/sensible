28a9a0bd556ea75bc4a0cc505e188228a83f4917
Ben <ben@stolovitz.com>
2020-04-13 15:53:37 -0400

    fix HTML proofer

--- _build/html_proof.sh
+++ _build/html_proof.sh
@@ -1,7 +1,8 @@
 htmlproofer ./_site \
     --internal_domains security.stolovitz.com \
-    # --check_favicon \
     --check_opengraph \
     --check_html \
     --check_img_http \
     --enforce_https \
+
+ # --check_favicon \
